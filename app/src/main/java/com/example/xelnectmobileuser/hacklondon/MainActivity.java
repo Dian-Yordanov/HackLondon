902bf774abde22fc27ce7e0671fb043c88fbba31
Dian Yordanov <dido.jordanov7@gmail.com>
2015-02-28 21:01:59 +0000

    restarted the app

--- app/src/main/java/com/example/xelnectmobileuser/hacklondon/MainActivity.java
+++ app/src/main/java/com/example/xelnectmobileuser/hacklondon/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 import android.webkit.WebView;
 
 
@@ -14,19 +16,28 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_launch);
+        setContentView(R.layout.activity_main);
 
+       /* this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //set content view AFTER ABOVE sequence (to avoid crash)
+        this.setContentView(R.layout.layout_launch);
+*/
+/*
         WebView webview = (WebView) findViewById(R.id.webView);
-        webview.loadUrl("http://www.just-eat.co.uk/");
-        //setContentView(webview);
+        webview.loadUrl("https://mcsnap.herokuapp.com");*/
 
-        //committest
+
+
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
@@ -40,10 +51,10 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-
-            Uri uri = Uri.parse("http://www.just-eat.co.uk/");
+/*
+            Uri uri = Uri.parse("https://mcsnap.herokuapp.com");
             Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-            startActivity(intent);
+            startActivity(intent);*/
 
             return true;
         }
